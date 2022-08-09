8d3f6b5a1b808a5ec1dda0a73947bac4c8800f3f
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2022-08-08 22:21:21 -0300

    chore: Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine} from 'phosphor-react'
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -5,13 +6,16 @@ export function Sidebar() {
         <aside className={styles.sidebar}>
             <img
                 className={styles.cover}
-                src="https://images.unsplash.com/photo-1566837945700-30057527ade0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"       />
+                src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=480&q=50"       />
+           
             <div className={styles.profile}>
-                <strong>Person Name</strong>
-                <span>Frontend</span>
+                <img className={styles.avatar}src="https://github.com/CiceroGB.png" alt="Profile image" />
+                <strong>Cicero Brunelli</strong>
+                <span>Full Stack Developer</span>
             </div>
             <footer>
                 <a>
+                <PencilLine  size={20} />
                     Edit your profile
                 </a>
             </footer>
