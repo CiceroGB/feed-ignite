96145a69b8975bf455512151d98718251c0125f6
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2022-08-08 22:05:47 -0300

    chore: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 import './global.css'
+import styles from './App.module.css'
 
 export function App() {
   return (
     <>
-      <Header/>
-      <h1>hello</h1>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <p>hello</p>
+          <p>hello</p>
+          <p>hello</p>
+        </main>
+
+      </div>
     </>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1566837945700-30057527ade0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"       />
+            <div className={styles.profile}>
+                <strong>Person Name</strong>
+                <span>Frontend</span>
+            </div>
+            <footer>
+                <a>
+                    Edit your profile
+                </a>
+            </footer>
+        </aside>
+    )
+}
