c02cf9fffa40bd3d392ccb167b6158be368faf14
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2022-11-15 14:39:11 -0300

    chore: delete comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -32,17 +32,12 @@ export function Post({ author, publishedAt, content }) {
         setNewCommentText(event.target.value);
     }
 
-
-
-
-
     function deleteComment(commentToDelete) {
-        const commentsWithoutDeletedOne = comments.filter(comment => {
-            return comment !== commentToDelete;
-        })
-
+        const commentsWithoutDeletedOne = comments.filter(comment => comment !== commentToDelete)
         setComments(commentsWithoutDeletedOne);
     }
+
+    
 
     return (
         <article className={styles.post}>
